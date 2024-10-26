f4887d933c62a58752b33ee24d04ff6ec89a3f59
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-10-26 15:21:34 +0200

    Update pfs1_postinstall.sh

--- Skripte_ZFS/pfSense/pfs1_postinstall.sh
+++ Skripte_ZFS/pfSense/pfs1_postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 qm set 1000 -delete ide2
 qm set 1000 -boot order=virtio0
-qm set 1000 -net0 model=virtio,bridge=vmbr0,firewall=0
+qm set 1000 -net0 model=virtio,bridge=vmbr0,firewall=0,mtu=1450,macaddr="00:24:18:0A:1B:DE"
 qm set 1000 -onboot 1
 
 
