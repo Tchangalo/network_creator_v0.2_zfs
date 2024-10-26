d084eac17b2f71d754ec0c82eb8be0a67600cd2c
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-10-26 15:24:35 +0200

    Update pfs3_postinstall.sh

--- Skripte_ZFS/pfSense/pfs3_postinstall.sh
+++ Skripte_ZFS/pfSense/pfs3_postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 qm set 3000 -delete ide2
 qm set 3000 -boot order=virtio0
-qm set 3000 -net0 model=virtio,bridge=vmbr0,firewall=0
+qm set 3000 -net0 model=virtio,bridge=vmbr0,firewall=0,mtu=1450,macaddr="00:24:18:0A:3B:DE"
 qm set 3000 -onboot 1
 
 
