03f04ee52ffce1c03906c5565aae303fb77b9d41
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-10-26 16:28:54 +0200

    Update dhcp2.sh

--- Skripte_ZFS/dhcp/dhcp2.sh
+++ Skripte_ZFS/dhcp/dhcp2.sh
@@ -3,3 +3,4 @@ qm create 207999 --name "dhcp2" --ostype l26 --memory 1536 --balloon 1500 --cpu 
 qm set 207999 --ide2 /var/lib/vz/template/iso/ubuntu-24.04.1-live-server-amd64.iso,media=cdrom
 qm set 207999 --boot order=ide2
 qm set 207999 -net1 model=virtio,bridge=vmbr1001,firewall=0
+qm set 107999 --agent enabled=1
